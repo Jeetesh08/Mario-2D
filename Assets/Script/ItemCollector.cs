973eaee726794b832642f73378e85fbe6f9c9b27
Jeetesh08 <112420368+Jeetesh08@users.noreply.github.com>
2023-03-25 02:40:55 +0530

    update

---
+++ Assets/Script/ItemCollector.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemCollector : MonoBehaviour
+{
+
+    private int charries = 0;
+    [SerializeField] private TMPro.TMP_Text charriesText;
+
+    [SerializeField] private AudioSource collectSoundEffect;
+        
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Cherry"))
+        {
+            collectSoundEffect.Play();
+            Destroy(collision.gameObject);
+            charries++;
+            charriesText.text = "Charries : " + charries;
+            
+        }
+    }
+}
